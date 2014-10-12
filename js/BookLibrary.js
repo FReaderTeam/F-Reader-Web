2e70c8f56b428c3b06096f0131e916c36912529d
Dmitry Yakutkin <paparastor@gmail.com>
2014-10-12 18:06:52 +0300

    Medium refactoring

---
+++ js/BookLibrary.js
@@ -0,0 +1,46 @@
+function BookLibrary() {
+
+    this.books = [];
+
+    var _this = this;
+
+
+
+    this.listBooks = function(on_success) {
+
+    };
+
+    this.getBookById = function(book_id) {
+
+    };
+
+
+    this.loadBookFile = function(file, callback, type) {
+
+    };
+
+
+    // Save book_body (xml content) into localStorage
+    this.storeBook = function(book, on_success) {
+
+    };
+
+
+    this.saveReadingPosition = function(book_id, chapter, position) {
+
+    }
+
+
+
+
+
+    this.getBookContent = function(book_id, on_success) {
+
+    };
+
+    // Delete book from DB
+    this.deleteBook = function(id, on_success) {
+
+    };
+
+}
---
+++ js/BookLibraryView.js
@@ -0,0 +1,13 @@
+function BookLibraryView(){
+    this.fillBookList  = function(books){
+        for (i = 0; i < books.length; i++) {
+            var bookName = books[i].name;
+            var bookPath = books[i].path;
+            var bookSize = books[i].size;
+            $(".list-group")
+                .append("<a href=\"book_view.html#" + books[i].path + "\" " +
+                    "class=\"list-group-item\"" +
+                    " onclick=\"openBook(this)\">" + books[i].name + "</a>");
+        }
+    }
+}
