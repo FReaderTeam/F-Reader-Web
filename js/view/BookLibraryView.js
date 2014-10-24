d245cbe514c584e2182da2d0984c894bc6582189
Dmitry Yakutkin <paparastor@gmail.com>
2014-10-24 18:38:09 +0300

    Minor refactoring stuff.

--- js/view/BookLibraryView.js
+++ js/view/BookLibraryView.js
@@ -1,5 +1,5 @@
-function BookLibraryView(){
-    this.fillBookList  = function(books){
+function BookLibraryView() {
+    this.fillBookList  = function(books) {
         for (var i = 0; i < books.length; i++) {
             var bookName = books[i].name;
             var bookPath = books[i].path;
@@ -12,3 +12,41 @@ function BookLibraryView(){
         $("body").removeClass("loading");
     }
 }
+
+$( document ).ready(function() {
+            // List filling.
+            var client = new DropboxClient();
+			client.authentificate(onSuccessComplete);
+            function onSuccessComplete() {
+                client.findFb2Files(new BookLibraryView().fillBookList);
+            }
+
+            // Uploading stuff.
+            var uploadForm = document.getElementById('upload-form');
+            var fileSelect = document.getElementById('file-select');
+            var uploadButton = document.getElementById('upload-button');
+            var uploads_count = 0;
+            uploadForm.onsubmit = function(event) {
+                event.preventDefault();
+                var files = fileSelect.files;
+                var full_path = 'FReaderBooks/';
+                $(document).bind("upload_done",
+                        function(){
+                            if (uploads_count == files.length) {
+                                location.reload();
+                            }
+                        }
+                );
+                for (var i = 0; i < files.length; i++) {
+                    console.log(files[i]);
+                    client.writeFile(full_path + files[i].name, files[i],
+                            function(fileName, requestState){
+                                uploads_count++;
+                                $(document).trigger('upload_done');
+                                // Uploading status pop-up.
+                                $( ".info" ).html(fileName);
+                                $('.info').stop().fadeIn(400).delay(3000).fadeOut(400);
+                            });
+                }
+            }
+        });
--- js/view/BookView.js
+++ js/view/BookView.js
@@ -1,4 +1,4 @@
-function BookView(){
+function BookView() {
     this.show = function(book_content,book_stat){
         var book = new Parser().getBookFromXml(book_content,book_stat);
         $('.b-reading').xslt(book.string_book_xml,'fb2-to-html.xsl');
@@ -16,3 +16,29 @@ function BookView(){
         });
     }
 }
+
+function onSuccessComplete() {
+	var full_path = window.location.hash.substr(1);
+	client.getDatastore(
+		function(datastore){
+			var positionDao = new PositionDao(datastore);
+			var posFinder = new PositionFinder();
+			var timer;
+			_datastore = datastore
+			$( window ).scroll(function() {
+				if ( timer ) clearTimeout(timer);
+				timer = setTimeout(function(){
+					var position = posFinder.findCurrentChapterAndPosition();
+					console.log(position);
+					positionDao.savePosition(position.section,position.position,full_path);
+				}, 200);
+			});
+			client.readBook(full_path, new BookView().show);
+		}
+	);
+}
+
+var client = new DropboxClient();
+client.authentificate(onSuccessComplete);
+var _datastore;
+
